d4f5e59122fbbf10d36f836bb1ffd673a85cede1
Oliver Aveyard <oliver@citruslime.com>
2025-12-18 15:37:55 +0000

    add vuels

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,6 +90,33 @@ return {
 			}),
 		})
 
+		vim.lsp.config("ts_ls", {
+			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vim.fn.stdpath("data")
+							.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+						languages = { "vue" },
+					},
+				},
+			},
+		})
+		vim.lsp.config("vue_ls", {
+			init_options = {
+				plugins = {
+					{
+						-- Name of the TypeScript plugin for Vue
+						name = "@vue/typescript-plugin",
+
+						-- Specify the languages the plugin applies to (in this case, Vue files)
+						languages = { "vue" },
+					},
+				},
+			},
+		})
+
 		vim.lsp.config("lua_ls", {
 			settings = {
 				Lua = {
